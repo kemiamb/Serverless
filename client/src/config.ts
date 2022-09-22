b7a25750e67ae4c09b61f24f0448eb8cd1d6b2d6
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-22 21:25:23 +0100

    app up and working

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'wi2yhu2pql';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`;
+
+export const authConfig = {
+  // Auth0 settings
+  domain: 'dev-oscferkz.us.auth0.com',
+  clientId: '1CwsujHi1Uli6vKxmZHCH02yaVsB1fWx',
+  callbackUrl: 'http://localhost:3000/callback'
+};
